2c41b6e02c15457be075d51b61be762161d61729
mentttobe15 <48445012+mentttobe15@users.noreply.github.com>
2019-03-11 14:52:04 +0000

    remove 5

--- nodeProject/src/db.js
+++ nodeProject/src/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/teste');
  
 var db = mongoose.connection;
- //MANUEL GAY
+ 
     db.on('error', console.error.bind(console, 'connection error'));
     db.once('open', function(callback) {
     console.log('Connection succeeded.');
