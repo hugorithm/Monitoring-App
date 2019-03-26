fc83a078e7b783572280fe70df0639b2486d46c2
diogohd8 <diogorodriges8@gmail.com>
2019-03-26 16:20:36 +0000

    moar stuff

--- nodeProject/server/app.js
+++ nodeProject/server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+
 const servicos = require('./funcs/servicos');
 const logs = require('./database/funcoes_logs');
-const controlo = require('./funcoes_controlo');
+const controlo = require('./database/funcoes_controlo');
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
@@ -24,11 +24,11 @@ const io = require("socket.io")(server);
 
 /*
 var servico = new Object();
-servico.nome = "facebook"
+servico.nome = "youtube"
 servico.tipo = "website"
 servico.class = "dunno"
 servico.propriedade = "wut"
-servico.tempo_verificacao = "10"
+servico.tempo_verificacao = "1"
 controlo.criar_servico(servico);
 */
 
@@ -50,8 +50,9 @@ function startup() {
             var tempo = a[i].tempo_verificacao;
 
             var trolha = nome + ".com";
+            var crontime = toCron(tempo);
             //iniciacao de servicos de monitorizacao por cada elemento
-            servicos.verificar_disponibilidade(tipo, trolha, io, tempo);
+            servicos.verificar_disponibilidade(tipo, trolha, io, crontime);
         }
     })
 }
--- nodeProject/server/funcs/ping_request.js
+++ nodeProject/server/funcs/ping_request.js
@@ -1,23 +1,32 @@
 var ping = require("net-ping");
-const teste = require('../database/funcoes_logs');
+const logs = require('../database/funcoes_logs');
 const dns = require('dns');
+var session = ping.createSession();
 
 var exports = module.exports = {}
-
-var session = ping.createSession();
 
 exports.send_ping = function (target, io) {
     dns.lookup(target, (err, address) => {
         session.pingHost(address, function (error, address, sent, rcvd) {
             var ms = rcvd - sent;
-            msf = "" + ms;
             if (error) {
-                msf = -1;
-                io.emit('ping_time', { username: target, pingtime: msf });
+                ms = -1;
+                io.emit('ping_time', { username: target, pingtime: ms });
             }
             else {
-                io.emit('ping_time', { username: target, pingtime: msf });
+                io.emit('ping_time', { username: target, pingtime: ms });
+                var obj = buildObj(target, ms, sent, rcvd);
+                logs.create_user(obj);
             }
         });
     });
+}
+
+function buildObj(nome, ping, data_enviado, data_recebido) {
+    var obj = new Object();
+    obj.nome = nome;
+    obj.ping = ping;
+    obj.data_enviado = data_enviado;
+    obj.data_recebido = data_recebido;
+    return obj;
 }
