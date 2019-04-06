2d6c154b3b03bbc3f0c779a7de41afb2e1a421e1
jpba69 <jota57@live.com.pt>
2019-04-06 19:30:08 +0100

    upload react

--- nodeProject/server/funcs/ping_request.js
+++ nodeProject/server/funcs/ping_request.js
@@ -1,32 +1,31 @@
 var ping = require("net-ping");
-const logs = require('../database/funcoes_logs');
-const dns = require('dns');
+const logs = require("../database/funcoes_logs");
+const dns = require("dns");
 var session = ping.createSession();
 
-var exports = module.exports = {}
+var exports = (module.exports = {});
 
-exports.send_ping = function (target, io) {
-    dns.lookup(target, (err, address) => {
-        session.pingHost(address, function (error, address, sent, rcvd) {
-            var ms = rcvd - sent;
-            if (error) {
-                ms = -1;
-                io.emit('ping_time', { username: target, pingtime: ms });
-            }
-            else {
-                io.emit('ping_time', { username: target, pingtime: ms });
-                var obj = buildObj(target, ms, sent, rcvd);
-                logs.create_user(obj);
-            }
-        });
+exports.send_ping = function(target, io) {
+  dns.lookup(target, (err, address) => {
+    session.pingHost(address, function(error, address, sent, rcvd) {
+      var ms = rcvd - sent;
+      if (error) {
+        ms = -1;
+        io.emit("ping_time", { username: target, pingtime: ms });
+      } else {
+        io.emit("ping_time", { username: target, pingtime: ms });
+        var obj = buildObj(target, ms, sent, rcvd);
+        //logs.create_user(obj);
+      }
     });
-}
+  });
+};
 
 function buildObj(nome, ping, data_enviado, data_recebido) {
-    var obj = new Object();
-    obj.nome = nome;
-    obj.ping = ping;
-    obj.data_enviado = data_enviado;
-    obj.data_recebido = data_recebido;
-    return obj;
+  var obj = new Object();
+  obj.nome = nome;
+  obj.ping = ping;
+  obj.data_enviado = data_enviado;
+  obj.data_recebido = data_recebido;
+  return obj;
 }
