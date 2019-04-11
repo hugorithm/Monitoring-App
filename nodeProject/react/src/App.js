e0e847a1bfc1645b2278499554e8eee837d94eb2
diogohd8 <diogorodriges8@gmail.com>
2019-04-11 18:35:42 +0100

    first merdas

--- nodeProject/react/src/App.js
+++ nodeProject/react/src/App.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
 import Menu from "./menu";
+import { connect, ping_time } from "./socket";
+
 class App extends Component {
+
+  constructor() {
+    this.state = {
+      data: [],
+      newPing:''
+    }
+
+      
+    this.setState({ 
+      pings: [] });
+    connect(message => {
+      //dunno
+    })
+    ping_time(data => {
+      var nome = data.username;
+      var a = [];
+
+      this.setState({pings})
+    })
+  }
+
+
+  handler(nome, data, ping) {
+    this.setState({ nome: [this.state.nome, data + ping] });
+  }
+  handler = this.handler.bind(this);
+
   render() {
     let links = [
       { label: "Home", link: "#home" },
@@ -11,8 +40,23 @@ class App extends Component {
     return (
       <div className="container center">
         <Menu links={links} />
+
+        {this.state.pings === null &&
+          <h1>empty</h1>
+        }
+        {this.state.pings !== null &&
+          <Dados pings={this.state.pings} />
+        }
       </div>
     );
   }
 }
 export default App;
+
+class Dados extends Component {
+  render() {
+    return (<div classname="dados">
+      <h1>ping</h1>
+    </div>);
+  }
+}
