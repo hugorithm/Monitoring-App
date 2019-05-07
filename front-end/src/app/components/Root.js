dbff51f025468687f0716debdb915aaa125d415c
prophecyyy <hugofsilv@gmail.com>
2019-05-07 23:56:28 +0100

    Fix spaces

--- front-end/src/app/components/Root.js
+++ front-end/src/app/components/Root.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-import {Header} from "./Header";
+import { Header } from "./Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class Root extends React.Component {
     render() {
         return (
             <div>
-            <div className="header">
-               <Header/>
-               </div>
+                <div className="header">
+                    <Header />
+                </div>
                 <div className="body">
-                    
-                        {this.props.children}
-
+                    {this.props.children}
                 </div>
-                </div>
+            </div>
         );
     }
 }
