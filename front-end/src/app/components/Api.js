35feb014f55aaca56753e724d9b8341ce7e3d973
prophecyyy <hugofsilv@gmail.com>
2019-05-08 00:54:27 +0100

    Update Api forms

--- front-end/src/app/components/Api.js
+++ front-end/src/app/components/Api.js
@@ -2,25 +2,29 @@ import React from "react";
 
 
 export class Api extends React.Component {
-   
+
 
     render() {
         return (
-            <form onSubmit="">
-            <div className="form-group">
-                <label>Nome:</label>
-                <input type="text" className="form-control" placeholder="Nome" />               
+            <div className="row">
+                <div className="col-md-2 col-md-offset-5">
+                    <form onSubmit="">
+                        <div className="form-group">
+                            <label>Nome:</label>
+                            <input type="text" className="form-control" placeholder="Nome" />
+                        </div>
+                        <div className="form-group">
+                            <label>Endereço:</label>
+                            <input type="text" className="form-control" placeholder="Endereço" />
+                        </div>
+                        <div className="form-group">
+                            <label>Tempo de Ciclo:</label>
+                            <input type="text" className="form-control" placeholder="Tempo de Ciclo" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div className="form-group">
-                <label>Endereço:</label>
-                <input type="text" className="form-control" placeholder="endereco" />               
-            </div>
-            <div className="form-group">
-                <label>Nome:</label>
-                <input type="text" className="form-control" placeholder="Nome" />               
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
         );
     }
 }
